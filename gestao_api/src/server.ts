aac15d0531c5bf110cd2553fa21dcb606ef38787
Vitor Barros <webdev.vitor@gmail.com>
2024-03-07 21:25:06 -0300

    Implements logger
    .
    
    .

--- gestao_api/src/server.ts
+++ gestao_api/src/server.ts
@@ -2,13 +2,22 @@ import express from "express";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import appRouter from "./routes/routes";
 import cors from "cors";
+import logger from "./logger";
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(cors({ origin: "*" }));
-app.use("/trpc", createExpressMiddleware({ router: appRouter }));
+app.use(
+  "/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    onError: ({ error, path }) => {
+      logger.error(`url: ${path}, error: ${error.message}`);
+    },
+  }),
+);
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
