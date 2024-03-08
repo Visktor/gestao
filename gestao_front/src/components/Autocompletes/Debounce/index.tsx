f96d7420838eaa7331104fab88f129b9dbb21a51
Vitor Barros <webdev.vitor@gmail.com>
2024-03-07 21:28:11 -0300

    Creates base debounce autocomplete

--- gestao_front/src/components/Autocompletes/Debounce/index.tsx
+++ gestao_front/src/components/Autocompletes/Debounce/index.tsx
@@ -5,11 +5,14 @@ import {
   TextField,
   TextFieldProps,
   CircularProgress,
+  AutocompleteInputChangeReason,
 } from "@mui/material";
-import useDebounce from "src/hooks/useDebounce";
+import useDebounce from "#hooks/useDebounce";
+import { ExtractKeysOfType } from "src/@types/util";
+import { useState } from "react";
 
 export default function AutocompleteDebounce<
-  T,
+  T extends Record<any, any>,
   M extends boolean,
   DC extends boolean,
 >(props: {
@@ -28,16 +31,24 @@ export default function AutocompleteDebounce<
     renderInput?: AutocompleteProps<T, M, DC, false, any>["renderInput"];
   };
   debounce: {
-    callback: () => void;
+    callback: (debouncedValue: string, abortSignal: AbortSignal) => void;
     timeout?: number;
     error?: boolean;
     pending?: boolean;
   };
-  textField?: TextFieldProps<"outlined">;
+  textField?: Omit<TextFieldProps<"outlined">, "variant">;
+  valueField: ExtractKeysOfType<T, string | number>;
+  labelField: ExtractKeysOfType<T, string>;
 }) {
+  const [lastInputChangeReason, setLastInputChangeReason] =
+    useState<AutocompleteInputChangeReason>("input");
+
   useDebounce({
     callback: props.debounce.callback,
-    error: props.debounce.error,
+    error:
+      lastInputChangeReason === "reset" ||
+      lastInputChangeReason === "clear" ||
+      props.debounce.error,
     timeout: props.debounce.timeout,
     depValue: props.autocomplete.inputValue,
   });
@@ -45,29 +56,38 @@ export default function AutocompleteDebounce<
   return (
     <Autocomplete
       {...props.autocomplete}
+      getOptionLabel={(opt) => opt[props.labelField]}
+      isOptionEqualToValue={(opt, val) =>
+        opt[props.valueField] === val[props.valueField]
+      }
+      onInputChange={(e, newValue, reason) => {
+        setLastInputChangeReason(reason);
+        props.autocomplete.onInputChange(e, newValue, reason);
+      }}
+      clearOnEscape={false}
       renderInput={
         props.autocomplete.renderInput
           ? props.autocomplete.renderInput
           : (riProps) => {
-            return (
-              <TextField
-                {...riProps}
-                {...props.textField}
-                InputProps={
-                  props.debounce.pending ||
-                    !props.autocomplete.options?.length
-                    ? {
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <CircularProgress />
-                        </InputAdornment>
-                      ),
-                    }
-                    : riProps.InputProps
-                }
-              />
-            );
-          }
+              return (
+                <TextField
+                  {...riProps}
+                  {...props.textField}
+                  variant="outlined"
+                  InputProps={
+                    props.debounce.pending
+                      ? {
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <CircularProgress size={20} />
+                            </InputAdornment>
+                          ),
+                        }
+                      : riProps.InputProps
+                  }
+                />
+              );
+            }
       }
     />
   );
